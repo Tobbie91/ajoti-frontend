73983d6729e62776b64cbe9293cdd3e30a9cfc2a
Pankyo(PK) Song <spkcome@gmail.com>
2026-01-30 15:17:29 -0700

    made a new page for createNewWallet second page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AppLayout } from '@/layouts'
 import { Home } from '@/pages'
+import { CreateNewWallet2 } from '@/pages'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <AppLayout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/createNewWallet2" element={<CreateNewWallet2 />} />
         </Routes>
       </AppLayout>
     </BrowserRouter>
