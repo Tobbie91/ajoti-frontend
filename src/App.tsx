13a2d9faebdedcb3c2788648cc3b31f560be57af
codekingibk <gboyegaibk@gmail.com>
2026-02-04 13:18:52 +0100

    Fix home layout routing and add rosca/investments pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { AuthProvider } from '@/components/AuthProvider'
-import { Home, Login, Signup, CreateNewWallet2 } from '@/pages'
+import { AppLayout } from '@/layouts'
+import { Home, Login, Signup, CreateNewWallet2, Rosca, Investments } from '@/pages'
 
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 const hasGoogleClientId = Boolean(googleClientId)
@@ -11,11 +12,48 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <AppLayout>
+                <Home />
+              </AppLayout>
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <AppLayout>
+                <Home />
+              </AppLayout>
+            }
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/createNewWallet2" element={<CreateNewWallet2 />} />
+          <Route
+            path="/createNewWallet2"
+            element={
+              <AppLayout>
+                <CreateNewWallet2 />
+              </AppLayout>
+            }
+          />
+          <Route
+            path="/rosca"
+            element={
+              <AppLayout>
+                <Rosca />
+              </AppLayout>
+            }
+          />
+          <Route
+            path="/investments"
+            element={
+              <AppLayout>
+                <Investments />
+              </AppLayout>
+            }
+          />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
