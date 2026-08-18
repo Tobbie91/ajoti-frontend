e4a910360e91eb2e6c9a3ca7cd8c885d61387ebc
Tolu Afolayan <31620246+tolukusan@users.noreply.github.com>
2026-08-18 08:08:47 +0100

    fix: require transaction PIN before withdrawal

--- apps/user/src/App.tsx
+++ apps/user/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { AuthProvider } from '@/components/AuthProvider'
 import { KycGate } from '@/components/KycGate'
+import { TransactionPinGate } from '@/components/TransactionPinGate'
 import { AppLayout, BlogLayout } from '@/layouts'
 import { Home, Login, Signup, CreateNewWallet2, Rosca, GroupDetails, RequestToJoin, JoinSummary, MyGroupRequests, InviteAccept, MyInvites, Investments, CreateNewWallet, VerifyOtp, GrowthActivities, BecomeAdmin, HowItWorks, ArticleDetail, Kyc, FundWallet, FundWalletCallback, WithdrawFunds, Transactions, Loans, MyDebts, Profile, ForgotPassword, ResetPassword, SetPin, Messages, Support, SupportTicket, Maintenance } from '@/pages'
 
@@ -15,233 +16,56 @@ function App() {
         <Routes>
           <Route path="/maintenance" element={<Maintenance />} />
           <Route path="/" element={<Login />} />
-          <Route
-            path="/home"
-            element={
-              <AppLayout>
-                <Home />
-              </AppLayout>
-            }
-          />
+          <Route path="/home" element={<AppLayout><Home /></AppLayout>} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/verify-otp" element={<VerifyOtp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/kyc" element={<Kyc />} />
-
-          <Route
-            path="/create-wallet"
-            element={
-              <AppLayout>
-                  <CreateNewWallet />
-                </AppLayout>
-            }
-          />
-
-          <Route
-            path="/createNewWallet2"
-            element={
-              <AppLayout>
-                  <CreateNewWallet2 />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/rosca"
-            element={
-              <AppLayout>
-                  <Rosca />
-                </AppLayout>
-            }
-          />
-          {/* Static /rosca/* routes must come before /rosca/:id */}
-          <Route
-            path="/rosca/how-it-works"
-            element={
-              <BlogLayout>
-                <HowItWorks />
-              </BlogLayout>
-            }
-          />
-          <Route
-            path="/rosca/how-it-works/:articleId"
-            element={
-              <BlogLayout>
-                <ArticleDetail />
-              </BlogLayout>
-            }
-          />
-          <Route
-            path="/rosca/become-admin"
-            element={
-              <AppLayout>
-                  <BecomeAdmin />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/rosca/requests"
-            element={
-              <AppLayout>
-                  <MyGroupRequests />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/rosca/invites"
-            element={
-              <AppLayout>
-                <MyInvites />
-              </AppLayout>
-            }
-          />
-          <Route
-            path="/rosca/invite/:token"
-            element={
-              <AppLayout>
-                <InviteAccept />
-              </AppLayout>
-            }
-          />
-          {/* Dynamic routes after all static ones */}
-          <Route
-            path="/rosca/:id/join"
-            element={
-              <AppLayout>
-                  <RequestToJoin />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/rosca/:id/summary"
-            element={
-              <AppLayout>
-                  <JoinSummary />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/rosca/:id/activities"
-            element={
-              <AppLayout>
-                  <GrowthActivities />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/rosca/:id"
-            element={
-              <AppLayout>
-                  <GroupDetails />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/investments"
-            element={
-              <AppLayout>
-                  <Investments />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/fund-wallet"
-            element={
-              <AppLayout>
-                <KycGate action="fund your wallet">
-                  <FundWallet />
-                </KycGate>
-              </AppLayout>
-            }
-          />
+          <Route path="/create-wallet" element={<AppLayout><CreateNewWallet /></AppLayout>} />
+          <Route path="/createNewWallet2" element={<AppLayout><CreateNewWallet2 /></AppLayout>} />
+          <Route path="/rosca" element={<AppLayout><Rosca /></AppLayout>} />
+          <Route path="/rosca/how-it-works" element={<BlogLayout><HowItWorks /></BlogLayout>} />
+          <Route path="/rosca/how-it-works/:articleId" element={<BlogLayout><ArticleDetail /></BlogLayout>} />
+          <Route path="/rosca/become-admin" element={<AppLayout><BecomeAdmin /></AppLayout>} />
+          <Route path="/rosca/requests" element={<AppLayout><MyGroupRequests /></AppLayout>} />
+          <Route path="/rosca/invites" element={<AppLayout><MyInvites /></AppLayout>} />
+          <Route path="/rosca/invite/:token" element={<AppLayout><InviteAccept /></AppLayout>} />
+          <Route path="/rosca/:id/join" element={<AppLayout><RequestToJoin /></AppLayout>} />
+          <Route path="/rosca/:id/summary" element={<AppLayout><JoinSummary /></AppLayout>} />
+          <Route path="/rosca/:id/activities" element={<AppLayout><GrowthActivities /></AppLayout>} />
+          <Route path="/rosca/:id" element={<AppLayout><GroupDetails /></AppLayout>} />
+          <Route path="/investments" element={<AppLayout><Investments /></AppLayout>} />
+          <Route path="/fund-wallet" element={<AppLayout><KycGate action="fund your wallet"><FundWallet /></KycGate></AppLayout>} />
           <Route path="/fund-wallet/callback" element={<FundWalletCallback />} />
           <Route
             path="/withdraw"
             element={
               <AppLayout>
                 <KycGate action="withdraw funds">
-                  <WithdrawFunds />
+                  <TransactionPinGate>
+                    <WithdrawFunds />
+                  </TransactionPinGate>
                 </KycGate>
               </AppLayout>
             }
           />
-          <Route
-            path="/transactions"
-            element={
-              <AppLayout>
-                  <Transactions />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/loans"
-            element={
-              <AppLayout>
-                  <Loans />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/debts"
-            element={
-              <AppLayout>
-                  <MyDebts />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <AppLayout>
-                  <Profile />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/set-pin"
-            element={
-              <AppLayout>
-                  <SetPin />
-                </AppLayout>
-            }
-          />
-          <Route
-            path="/messages"
-            element={
-              <AppLayout>
-                <Messages />
-              </AppLayout>
-            }
-          />
-          <Route
-            path="/support"
-            element={
-              <AppLayout>
-                <Support />
-              </AppLayout>
-            }
-          />
-          <Route
-            path="/support/:ticketId"
-            element={
-              <AppLayout>
-                <SupportTicket />
-              </AppLayout>
-            }
-          />
+          <Route path="/transactions" element={<AppLayout><Transactions /></AppLayout>} />
+          <Route path="/loans" element={<AppLayout><Loans /></AppLayout>} />
+          <Route path="/debts" element={<AppLayout><MyDebts /></AppLayout>} />
+          <Route path="/profile" element={<AppLayout><Profile /></AppLayout>} />
+          <Route path="/set-pin" element={<AppLayout><SetPin /></AppLayout>} />
+          <Route path="/messages" element={<AppLayout><Messages /></AppLayout>} />
+          <Route path="/support" element={<AppLayout><Support /></AppLayout>} />
+          <Route path="/support/:ticketId" element={<AppLayout><SupportTicket /></AppLayout>} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
   )
 
-  // Avoid initializing Google OAuth when no client ID is configured.
-  if (!hasGoogleClientId) {
-    return app
-  }
-
+  if (!hasGoogleClientId) return app
   return <GoogleOAuthProvider clientId={googleClientId}>{app}</GoogleOAuthProvider>
 }
 
 export default App
-
